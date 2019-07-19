8e53620c049ea8564dc7af31c12fa110d94e93fc
Victor Markov <vikata3@gmail.com>
2019-07-20 01:32:25 +0300

    Get configuration from external providers

--- app.js
+++ app.js
@@ -1,8 +1,54 @@
 const fs = require('fs');
+const http = require('http');
 const args = process.argv;
 const fileArgument = args.slice(2)[0];
+const CASH_OUT_JURIDICAL_PROVIDER = 'http://private-38e18c-uzduotis.apiary-mock.com/config/cash-out/juridical';
+const CASH_OUT_NATURAL_PROVIDER = 'http://private-38e18c-uzduotis.apiary-mock.com/config/cash-out/natural';
+const CASH_IN_PROVIDER = 'http://private-38e18c-uzduotis.apiary-mock.com/config/cash-out/natural';
+
+function wrapHttpRequestInPromise(url) {
+    return new Promise((resolve, reject) => {
+        http.get(url, res => {
+            res.setEncoding('utf8');
+            let rawData = '';
+            res.on('data', (chunk) => {
+                rawData += chunk;
+            });
+            res.on('end', () => {
+                try {
+                    const parsedData = JSON.parse(rawData);
+                    resolve(parsedData)
+                } catch (e) {
+                    reject(e);
+                }
+            });
+        });
+    });
+}
+
+async function getCashOutJuridicalConfig() {
+    return wrapHttpRequestInPromise(CASH_OUT_JURIDICAL_PROVIDER);
+}
+
+async function getCashOutNaturalConfig() {
+    return wrapHttpRequestInPromise(CASH_OUT_NATURAL_PROVIDER);
+}
+
+async function getCashInConfig() {
+    return wrapHttpRequestInPromise(CASH_IN_PROVIDER);
+}
+
+
+getCashOutJuridicalConfig();
 
 fs.readFile(fileArgument, 'utf8', (err, data) => {
     if (err) return;
-    const transactionData = JSON.parse(data);    
+    const transactionData = JSON.parse(data);
 })
+
+// parse data
+// validate data
+// sort the transactions by date
+// calculate transactions fees
+// -- persists user current transaction amount
+// -- reset transaction amount limit per week
