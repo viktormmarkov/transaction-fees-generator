f6e78b281a93ed4715a23c2b5c87f8b439d8b4a9
Victor Markov <vikata3@gmail.com>
2019-07-22 22:49:08 +0300

    wipe appjs

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-const InputDataProvider = require('./inputDataProvider');
-
-
-// getconfigdata
-// parse data
-// validate data
-// sort the transactions by date
+// load data
 // calculate transactions fees
 // -- persists user current transaction amount
 // -- reset transaction amount limit per week
