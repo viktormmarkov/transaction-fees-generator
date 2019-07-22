1fc4d127614a3f6462a38d92dd82c7ecda132351
Victor Markov <vikata3@gmail.com>
2019-07-22 23:15:44 +0300

    Add basic validations for the transaction

--- commisionFeesProcessor.js
+++ commisionFeesProcessor.js
@@ -1,22 +1,74 @@
+const _ = require('lodash');
+const {
+    VALID_OPERATIONS,
+    VALID_USER_TYPES,
+    SUPPORTED_CURRENCIES
+} = require('./transactionSchemaTypesCheck');
+
 class ComissionFeesProcessor {
     constructor(commissionFeesConfig) {
         this.commissionFeesConfig = commissionFeesConfig;
     };
 
+    isOperationTypeValid(type) {
+        return type && _.contains(VALID_OPERATIONS, type);
+    }
+
+    isUserTypeValid(userType) {
+        return type && _.contains(VALID_USER_TYPES, userType);
+    }
+
+    isCurrencyValid(currency) {
+        return currency && _.contains(SUPPORTED_CURRENCIES, currency);
+    }
+
+    isAmountValid(amount) {
+        return _.isFinite(amount);
+    }
+
     validateTransaction(transaction) {
-        const {type, user_type: userType, operation: { amount, currency }} = transaction;
+        const {
+            type,
+            user_type: userType,
+            operation: {
+                amount,
+                currency
+            }
+        } = transaction;
+        const isOperationTypeValid = this.isOperationTypeValid(type);
+        const isUserTypeValid = this.isUserTypeValid(userType);
+        const isCurrencyValid = this.isCurrencyValid(currency);
+        const isAmountValid = this.isAmountValid(amount);
+
+        // consider checking whether operation configuration has the same currency as the transaction
+        return isOperationTypeValid && isUserTypeValid && isCurrencyValid && isAmountValid;
     }
 
     generateFee(transaction) {
-        const {type, user_type: userType, operation: { amount, currency }} = transaction;
+        const {
+            type,
+            user_type: userType,
+            operation: {
+                amount,
+                currency
+            }
+        } = transaction;
         if (type === 'cash_in') {
-            const { percents, max: { amount: maxAmount, currency: cashInCurrency } } = this.commissionFeesConfig.cashIn;
+            const {
+                percents,
+                max: {
+                    amount: maxAmount,
+                    currency: cashInCurrency
+                }
+            } = this.commissionFeesConfig.cashIn;
             if (cashInCurrency === currency) {
                 return Math.min(percents * amount, maxAmount);
             }
         } else if (type === 'cash_out') {
-            if (userType === "natural") {
+            if (userType === 'natural') {
+
             } else if (userType === 'juridical') {
+
             }
         }
     }
