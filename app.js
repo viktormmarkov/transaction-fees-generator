22f9e36386afbba1b42cada39855b386c674d7ee
Victor Markov <vikata3@gmail.com>
2019-07-22 20:43:08 +0300

    Create base for fee generation

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
 const fs = require('fs');
 const args = process.argv;
 const fileArgument = args.slice(2)[0];
-const commissionFeesProvider = require('./commisionFeesProvider');
+const InputDataProvider = require('./inputDataProvider');
 
-async function printCommissionFees() {
-    const natural = await commissionFeesProvider.getCashOutNatural();
-    const juridical = await commissionFeesProvider.getCashOutJuridical();
-    const cashin = await commissionFeesProvider.getCashIn();
-    console.log(natural, juridical, cashin);
-};
-
-printCommissionFees();
-
-fs.readFile(fileArgument, 'utf8', (err, data) => {
-    if (err) return;
-    const transactionData = JSON.parse(data);
-})
 
 // getconfigdata
 // parse data
