0f4e568eede459da998c0b61dfbf65e73f27b966
Victor Markov <vikata3@gmail.com>
2019-07-24 00:07:51 +0300

    Fix eslint commissionFeesConfiguration provider

--- commisionFeesConfigurationProvider.js
+++ commisionFeesConfigurationProvider.js
@@ -1,22 +1,25 @@
-const { promisifyGetRequest } = require('./httpPromiseWrapper');
+const {
+  promisifyGetRequest,
+} = require('./httpPromiseWrapper');
+
 const CASH_OUT_JURIDICAL_URL = 'http://private-38e18c-uzduotis.apiary-mock.com/config/cash-out/juridical';
 const CASH_OUT_NATURAL_URL = 'http://private-38e18c-uzduotis.apiary-mock.com/config/cash-out/natural';
 const CASH_IN_URL = 'http://private-38e18c-uzduotis.apiary-mock.com/config/cash-out/natural';
 
 async function getCashOutJuridical() {
-    return promisifyGetRequest(CASH_OUT_JURIDICAL_URL);
+  return promisifyGetRequest(CASH_OUT_JURIDICAL_URL);
 }
 
 async function getCashOutNatural() {
-    return promisifyGetRequest(CASH_OUT_NATURAL_URL);
+  return promisifyGetRequest(CASH_OUT_NATURAL_URL);
 }
 
 async function getCashIn() {
-    return promisifyGetRequest(CASH_IN_URL);
+  return promisifyGetRequest(CASH_IN_URL);
 }
 
 module.exports = {
-    getCashOutJuridical,
-    getCashOutNatural,
-    getCashIn
-}
+  getCashOutJuridical,
+  getCashOutNatural,
+  getCashIn,
+};
