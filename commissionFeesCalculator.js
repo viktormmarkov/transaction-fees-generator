70675e4d1498a9de3967e8cc37feebd8abf1d440
Victor Markov <vikata3@gmail.com>
2019-07-25 00:49:30 +0300

    Add bussinesLogic for the cashoutJuridical

--- commissionFeesCalculator.js
+++ commissionFeesCalculator.js
@@ -1,15 +1,31 @@
 const _ = require('lodash');
+const moment = require('moment');
 const {
   VALID_OPERATIONS,
   VALID_USER_TYPES,
   SUPPORTED_CURRENCIES,
 } = require('./transactionSchemaTypesCheck');
 
+function getWeekNumber(date) {
+  const dateMoment = moment(date);
+  const weekday = dateMoment.weekday();
+  const week = dateMoment.week();
+  // moment week is from sunday to saturday
+  // cant find a way to change it
+  // good workaround for now
+  if (weekday === 0) {
+    return week - 1;
+  } else {
+    return week;
+  }
+}
+
 class CommissionFeesCalculator {
   constructor({ cashIn, cashOutNatural, cashOutJuridical }) {
     this.cashIn = cashIn;
     this.cashOutNatural = cashOutNatural;
     this.cashOutJuridical = cashOutJuridical;
+    this.userWeeklyTransactions = {};
   }
 
   isOperationTypeValid(type) {
@@ -46,34 +62,78 @@ class CommissionFeesCalculator {
     return isOperationTypeValid && isUserTypeValid && isCurrencyValid && isAmountValid;
   }
 
+  getCashInCommission({operation: {amount}}) {
+    const {
+      percents,
+      max: {
+        amount: maxAmount,
+      },
+    } = this.cashIn;
+    return Math.min(percents * amount / 100, maxAmount);
+  }
+
+  getCashOutJuridicalCommission({operation: {amount}}) {
+    const {
+      percents,
+      min: {
+        amount: minAmount,
+      },
+    } = this.cashOutJuridical;
+    return Math.max(percents * amount / 100, minAmount);
+  }
+
+  getCashOutNaturalCommission({operation: {amount}, user_id: userId, date}) {
+    const {
+      percents,
+      week_limit: {
+        amount: maxAmount,
+      },
+    } = this.cashOutNatural;
+    const totalWeeklyUserTransaction = this.getWeeklyUserTransaction(userId, date);
+    if (totalWeeklyUserTransaction > maxAmount) {
+      return percents * amount / 100;
+    } else if (totalWeeklyUserTransaction + amount > maxAmount) {
+      return Math.abs(maxAmount - totalWeeklyUserTransaction + amount) * percents / 100
+    } else {
+      return 0;
+    }
+  }
+
   generateFee(transaction) {
     const {
       type,
+      user_id: userId,
+      date,
       user_type: userType,
     } = transaction;
-    const {amount} = transaction.operation;
+    const { amount } = transaction.operation;
     let commissionAmount = 0;
     if (type === 'cash_in') {
-      const {
-        percents,
-        max: {
-          amount: maxAmount,
-        },
-      } = this.cashIn;
-      commissionAmount = Math.min(percents * amount / 100, maxAmount);
+      commissionAmount = this.getCashInCommission(transaction);
     } else if (type === 'cash_out' && userType === 'natural') {
-      commissionAmount = 0;
+      commissionAmount = this.getCashOutNaturalCommission(transaction);
+      this.updateWeeklyUserTransaction(userId, date, amount);
     } else if (type === 'cash_out' && userType === 'juridical') {
-      const {
-        percents,
-        min: {
-          amount: minAmount,
-        },
-      } = this.cashOutJuridical;
-      commissionAmount = Math.max(percents * amount / 100, minAmount);
+      commissionAmount = this.getCashOutJuridicalCommission(transaction);
     }
     return commissionAmount;
   }
+
+  getWeeklyUserTransaction(userId, date) {
+    const weekNumber = getWeekNumber(date);
+    return (this.userWeeklyTransactions[weekNumber] && this.userWeeklyTransactions[weekNumber][userId]) || 0;
+  }
+
+  updateWeeklyUserTransaction(userId, date, amount) {
+    const weekNumber = getWeekNumber(date);
+    if (this.userWeeklyTransactions[weekNumber]) {
+      const currentWeeklyAmount = this.userWeeklyTransactions[weekNumber][userId];
+      this.userWeeklyTransactions[weekNumber][userId] = currentWeeklyAmount + amount;
+    } else {
+      this.userWeeklyTransactions[weekNumber] = {};
+      this.userWeeklyTransactions[weekNumber][userId] = amount;
+    }
+  } 
 
   generateCommissionFees(transactions) {
     return _(transactions)
